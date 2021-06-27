c059166ddb84a67d497332068096800a2e62892b
RealShaniaTwain <47068172+RealShaniaTwain@users.noreply.github.com>
2021-06-27 11:18:22 -0700

    Added Expense.js and Card.js

---
+++ src/components/Expenses.js
@@ -0,0 +1,19 @@
+import ExpenseItem from "./ExpenseItem";
+import './Expenses.css'
+import Card from "./Card";
+
+let Expenses = (props) => {
+    return (
+        <Card className="expenses">
+            {props.expenses.map((expense) => (
+                <ExpenseItem
+                title={expense.title}
+                amount={expense.amount}
+                date={expense.date}
+            />
+            ))}
+        </Card>
+    )
+};
+
+export default Expenses;
